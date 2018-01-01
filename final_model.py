619ff57984603bc9c8c2eebf6efe4d54f0547275
Tanmoy Baidya <tanmoy.baidya.met14@itbhu.ac.in>
2018-01-01 07:09:39 +0530

    Update final_model.py

--- final_model.py
+++ final_model.py
@@ -1,18 +1,16 @@
+#Library Importing
 import numpy as np
 import pandas as pd
 import keras
 import tensorflow
 import sklearn
-
+#Data Importing
 data=pd.read_excel("Concrete_Data.xls")
 X=data.drop(['Concrete compressive strength(MPa, megapascals) '],axis=1)
 y=data['Concrete compressive strength(MPa, megapascals) ']
-"""data=pd.read_csv("Processed_Data.csv")
-X=data.drop(["Concrete_Compressive_Strength"],axis=1)
-y=data["Concrete_Compressive_Strength"]"""
 
-from keras.layers import Dense
-from keras.models import Sequential
+
+#Data Preprocessing
 
 from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2)
@@ -21,25 +19,23 @@ sc=StandardScaler()
 X_train=sc.fit_transform(X_train)
 X_test=sc.transform(X_test)
 
-
+#Model Initialization
+from keras.layers import Dense
+from keras.models import Sequential
 Neural_Network=Sequential()
 Neural_Network.add(Dense(units=4,activation='relu',kernel_initializer='uniform',input_dim=8))
 Neural_Network.add(Dense(units=4,activation='relu',kernel_initializer='uniform'))
-#Neural_Network.add(Dense(units=4,activation='relu',kernel_initializer='uniform'))
 Neural_Network.add(Dense(units=1,activation='relu',kernel_initializer='uniform'))
 Neural_Network.compile(optimizer='adam',loss='mean_squared_error')
 
-
-
-
-
+## Model Fitting and Prediction
 
 Neural_Network.fit(X_train,y_train,batch_size=32, epochs=100)
 pred=Neural_Network.predict(X_test)
 from sklearn.metrics import mean_squared_error
 print("\n Mean Squared Error:",mean_squared_error(y_test,pred))
 
-
+## Model Saving
 
 from keras.models import model_from_json
 
